71ae32dde6e0b6a51849410415e5da5d8bfba34f
ZuhairiAmran <zuhairi.amran.3103@gmail.com>
2025-10-24 08:03:59 +0800

    Bug Fix for BuffState

--- Assets/Scripts/BuffState.cs
+++ Assets/Scripts/BuffState.cs
@@ -4,24 +4,7 @@ public abstract class BuffState : ScriptableObject
 {
     public abstract void Tick(BuffStateController controller);
 
-    public virtual void OnEnter(BuffStateController controller)
-    {
-        if (stateAudio != null)
-        {
-            var audioSource = controller.GetComponent<AudioSource>();
-            if (audioSource != null)
-            {
-                audioSource.clip = stateAudio;
-                audioSource.Play();
-            }
-        }
-        
-        var sprite = controller.GetComponent<SpriteRenderer>();
-        if (sprite != null)
-        {
-            sprite.color = stateColor;
-        }
-    }
+    public virtual void OnEnter(BuffStateController controller) { }
     public virtual void OnExit(BuffStateController controller) { }
     public virtual void OnTimerEnd(BuffStateController controller) { }
 }
